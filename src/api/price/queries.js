a902ef7d579fe1695b03b8749cf458c35054b812
Ricardo Canastro <ricardo.canastro@blip.pt>
2016-10-30 21:19:34 +0000

    Change filter type for prices

--- src/api/price/queries.js
+++ src/api/price/queries.js
@@ -1,46 +1,50 @@
+const moment = require('moment');
 const {
     GraphQLString,
-    GraphQLInt
+    GraphQLList
 } = require('graphql');
 
 const Price = require('wolfy-models/src/schema/price');
-
-const { getFilter, applyPagination } = require('../utils/pagination');
-const EdgeType = require('../types/pagination/edge');
-const ConnectionType = require('../types/pagination/connection');
-const Cursor = require('../types/pagination/cursor');
-
 const PriceType = require('../types/prices/price');
-
-function getPrices({ symbol, first, last, before, after }, order) {
-    const filter = getFilter(before, after, order, { symbol });
-    const query = Price.find(filter).sort([['_id', order]]);
-
-    return Price.find(filter).count().then(count => {
-        const pageInfo = applyPagination(query, count, first, last);
-        return pageInfo.query.exec().then((results) => ({
-            query: results,
-            pageInfo: {
-                hasNextPage: pageInfo.hasNextPage,
-                hasPreviousPage: pageInfo.hasPreviousPage
-            }
-        }));
-    });
-}
-
-const PriceEdge = EdgeType('PriceEdge', PriceType);
-const PriceConnection = ConnectionType('PriceConnection', PriceEdge);
+const getProjection = require('../utils/get-projection');
 
 module.exports = {
     prices: {
-        type: PriceConnection,
+        type: new GraphQLList(PriceType),
         args: {
-            first: { type: GraphQLInt },
-            last: { type: GraphQLInt },
-            before: { type: Cursor },
-            after: { type: Cursor },
-            symbol: { type: GraphQLString }
+            since: {
+                name: 'since',
+                type: GraphQLString
+            },
+            until: {
+                name: 'until',
+                type: GraphQLString
+            },
+            symbol: {
+                name: 'symbol',
+                type: GraphQLString
+            }
         },
-        resolve: (parent, args) => getPrices(args, -1)
+        resolve(parent, args, context, info) {
+            const projection = getProjection(info);
+            const filter = {
+                symbol: args.symbol
+            };
+            const dateFilter = {};
+
+            if (args.since) {
+                dateFilter['$gte'] = moment(+args.since).toDate().toISOString();
+            }
+
+            if (args.until) {
+                dateFilter['$lte'] = moment(+args.until).toDate().toISOString();
+            }
+
+            if (Object.keys(dateFilter)) {
+                filter.date = dateFilter;
+            }
+
+            return Price.find(filter).sort('date').select(projection).exec();
+        }
     }
 };
